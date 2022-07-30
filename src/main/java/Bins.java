667997dd088782c3ed68feb1488b778eed905e72
tristan <jametri014@gmail.com>
2022-07-29 22:35:59 -0400

    Finished bin class

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,19 @@
 
 public class Bins {
+    int [] bin;
+
+    public Bins(int high) {
+        int [] arrBin = new int[high + 1];
+        this.bin = arrBin;
+    }
+
+    public void incrementBin(int index) {
+        bin[index] ++;
+    }
+
+    public Integer getBin(Integer index) {
+        return bin[index];
+    }
 
 }
+
